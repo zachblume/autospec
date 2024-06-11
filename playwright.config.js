2e6f3c2de9dc467c6e0511b9d7c06565f92b5d7e
Zach Blume <zachblume@gmail.com>
2024-06-10 20:00:19 -0400

    Make playwright run on only the trajectories folder in dev env

--- playwright.config.js
+++ playwright.config.js
@@ -2,5 +2,5 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
     // Look for test files in the "tests" directory, relative to this configuration file.
-    testDir: "tests",
+    testDir: "trajectories",
 });
