d49299109c9487973ee4efc5680705342dcd2d9a
Zach Blume <zachblume@gmail.com>
2024-06-28 09:22:54 -0400

    Revert "shift to playwright-core, bump version"
    This reverts commit e0297d93846b14c51c6caef1b0d169648b6698e7.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { z } from "zod";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import fs from "fs";
-import playwright from "playwright-core";
+import playwright from "playwright";
 import stripAnsi from "strip-ansi";
 import winston from "winston";
 
